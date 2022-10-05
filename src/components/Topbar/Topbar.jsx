7df5fb3a9f355fb48a868c297ac626f79fa88fb0
Princewill-Awili <princewill.awili@gmail.com>
2022-10-05 18:58:25 +0100

    m"Search mode functionality for mobile view completed"

--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -1,10 +1,24 @@
+import { useState, useContext } from 'react';
 import './topbar.scss';
 import {TbBooks} from 'react-icons/tb'
 import {FiSearch } from 'react-icons/fi'
 import {MdOutlineShoppingCart as CartIcon} from 'react-icons/md'
+import {BiArrowBack} from 'react-icons/bi'
+import {IoClose as Close} from 'react-icons/io5'
+
+import { states } from '../../context';
 
 
 const Topbar = () => {
+
+    const {query, setQuery} = useContext(states);
+
+    const [searchMode, setSearchMode] = useState(false);
+    const [cartMode, setCartMode] = useState(false);
+
+    const[dropHeight,setDropHeight] = useState(false);
+
+
   return (
     <div className='topbar'>
 
@@ -30,7 +44,13 @@ const Topbar = () => {
         <div className="tbRight">
 
             <div className="mobileIcons">
-                <FiSearch className='mbIcons cartIcon'/>
+                <FiSearch 
+                    className='mbIcons cartIcon' 
+                    onClick={()=>{
+                        setSearchMode(!searchMode);
+                        setDropHeight(!dropHeight)
+                    }}
+                />
                 <TbBooks className='mbIcons cartIcon'/>
                 <div className="cartIconWrapper">
                     <CartIcon className='mbIcons cartIcon'/>
@@ -42,6 +62,51 @@ const Topbar = () => {
                 <TbBooks className='logo2Icon'/>
             </div>
         </div>
+
+        {
+           searchMode && (
+            <div className="mobileSearch" style={{animationName: dropHeight ? "dropHeight" : "liftHeight"}}>
+                <BiArrowBack  
+                    className='arrowBack' 
+                    onClick={()=>{
+                        setDropHeight(!dropHeight);
+                        setTimeout(()=>{
+                            setSearchMode(!searchMode)
+                        },300)
+                        
+                    }} 
+                />
+                <div className="mobileSearchBarWrapper">
+                    <input 
+                        type="text" 
+                        className="mobileSearchBar" 
+                        placeholder='Books, authors, genres, etc.'
+                        value={query}
+                        onChange = {(e)=> setQuery(e.target.value)}
+                    />
+                    <div className="mobileSearchIconWrapper">
+                        
+                        {query.length === 0 && (<FiSearch  className='mobileSearchIcon'/>)}
+                        {
+                            query.length > 0 && (
+                                                    <Close 
+                                                        className='mobileSearchIcon' 
+                                                        onClick = {()=> setQuery('')}/>
+                                                )
+                        }
+                    </div>
+                </div>
+            </div>
+           ) 
+        }
+
+        {
+            cartMode && (
+                <div className="cartBar">
+                    
+                </div>
+            )
+        }
     </div>
   )
 }
--- src/context.js
+++ src/context.js
@@ -1,14 +1,15 @@
-import React, {createContext } from "react";
+import React, {createContext, useState } from "react";
 
 
 export const states = createContext();
 
 export const StateContextProvider = ({children}) => {
 
+    const [query, setQuery] = useState('')
     
 
     return (
-        <states.Provider value={{}}>
+        <states.Provider value={{query, setQuery}}>
             {children}
         </states.Provider>
     )
